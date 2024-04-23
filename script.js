c0ef1435668ee1732b5cefc1e7660e189dc15719
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-04-23 14:43:28 +0700

    menambahkan animasi flash pada gambar saat kita klik

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ container.addEventListener("click", function(e) {
     // cek apakah yang diklik adalah thumb
     if(e.target.className == "thumb"){
         jumbo.src = e.target.src;
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 500)
     }
 });
